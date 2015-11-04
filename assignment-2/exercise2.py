cfd24d06d056f26a75214703ce5dd255745a34bb
Inez <inezwijnands@gmail.com>
2015-11-04 16:45:55 +0100

    Exercise 2 boilerplate

---
+++ assignment-2/exercise2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Inez Wijnands s4149696 
+# Guido Zuidhof s4160703
+# SML ASS 2 exercise2.py
+
+from __future__ import division
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    
