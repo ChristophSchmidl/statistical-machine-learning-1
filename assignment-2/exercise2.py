5ae0f1e51fee6b4dc6907ee4841d29f54630dd92
Inez <inezwijnands@gmail.com>
2015-11-04 23:01:08 +0100

    Alpha plot

--- assignment-2/exercise2.py
+++ assignment-2/exercise2.py
@@ -9,8 +9,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def distr():
-    return [b / (math.pi*((b**2)+((_x-a)**2))) for _x in x]
+def distr_single(x,a,b):
+    return [b / (math.pi*(b**2+((_x-a)**2))) for _x in x]
+
+def distr_data(data,a,b):
+    return np.product([b / math.pi * (1/ ((d-a)**2 + b**2)) for d in data])
 
 
 def plot_distribution():
@@ -18,29 +21,27 @@ def plot_distribution():
     b = 1.0
     
     x = np.linspace(-10, 10, 1000)
-    plt.plot(x, distr(a,b,x), 'b-', lw=1.5)
+    plt.plot(x, distr(x,a,b), 'g-', lw=1.5)
     plt.xlabel (r'$x_k$')
     plt.ylabel(r'$p(x_k|\alpha,\beta)$')
     
 
 
-def log_posterior_density_plot():
+def plot_with_data():
     data = [4.8, -2.7, 2.2, 1.1, 0.8, -7.3]
+    b = 1
     
-    a_values = np.linspace(-5,-5,1000)
-    
-    
-    #for a in a_values:
-
-           # np.product()
-        
+    a_values = np.linspace(-5,5,1000)
+    y_values = [distr_data(data,a, b) for a in a_values]
     
     
-    pass
-    
-    
+    plt.plot(a_values, y_values, 'g-', lw=1.5) 
+    plt.xlabel (r'$\alpha$')
+    plt.ylabel(r'$p(\alpha|\mathcal{D},\beta=1)$')
+    plt.xlim(-5,5)
     
 
 
 if __name__ == "__main__":
-    plot_distribution()
+    #plot_distribution()
+    plot_with_data()
