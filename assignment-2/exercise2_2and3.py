a462251bd6046f02a590e3056c5e247a1b2874d6
Inez <inezwijnands@gmail.com>
2015-11-05 06:28:28 +0100

    Comments, non-cheat fmin

--- assignment-2/exercise2_2and3.py
+++ assignment-2/exercise2_2and3.py
@@ -8,19 +8,23 @@ import math
 import numpy as np
 import scipy
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+
 
 def generate_data(a,b,n):
     data = []
+    
     for _ in xrange(n):
+        #Angle of the light flash (theta)
         angle = np.random.uniform(0.5*-math.pi, 0.5*math.pi)
         value = b*math.tan(angle)+a
         data.append(value)
     
     return data
 
+
 def generate_random_position():
+    # position (alpha and beta) is drawn from a uniform distribution
     a = np.random.uniform(0.00,10.0)
     b = np.random.uniform(1.0,2.0)
     return a,b
@@ -29,7 +33,6 @@ def generate_random_position():
 
 def plot_mean_over_time(data):
     
-    
     actual_mean = sum(data)/len(data)
     print "Actual mean:", actual_mean
     
@@ -37,7 +40,8 @@ def plot_mean_over_time(data):
     x = xrange(len(data))
     for i in x:
         means.append( sum(data[:i+1])/(i+1))
-        
+    
+    #Make plot
     plt.plot(x, means, label="Running mean")
     plt.plot(x, [actual_mean]*len(data), 'c-',label="Actual mean")
     plt.legend()
@@ -48,14 +52,18 @@ def plot_mean_over_time(data):
 def plot_likelihood(data):
     a_grid,b_grid = np.meshgrid(np.linspace(-10,10,1000), np.linspace(0,5,1000))
     
+    #Colorbar lower bounds
     vmins = [-20,-30,-40,-240]    
     
     for k,v in zip([1,2,3,20],vmins):
         constant = k * np.log(b_grid/math.pi)
         log_llh = constant
+        
+        #Calculate for up to datapoint k+1
         for x_k in data[:k+1]:
             log_llh = log_llh - np.log((x_k - a_grid)**2 + b_grid**2)
     
+        #Plot
         fig = plt.figure(k)
         ax = fig.add_subplot(111, projection='3d')
         plt.title("Log likelihood for k = " + str(k))
@@ -64,8 +72,11 @@ def plot_likelihood(data):
         surf = ax.plot_surface(a_grid,b_grid, log_llh, cmap=cm.winter, vmin=v, vmax=0, linewidth=0.05,rstride=5, cstride=5)
         plt.colorbar(surf, shrink=1,aspect=30)
 
+
+#Function to minimize using "scipy.optimize.fmin" (instead of fminsearch in MATLAB)
 def log_likelihood(x, data):
     a,b = x
+    
     constant = len(data) * np.log(b/math.pi)
     log_llh = constant
     for x_k in data:
@@ -79,13 +90,16 @@ def plot_likelihood_function_of_k(data,a,b):
     a_values = []
     b_values = []
     
-    k_values = range(len(data))   
-    
-    for _k in k_values:
+    #K values to plot on the x-axis
+    k_values = range(1,len(data)+1) 
+
+    for k in k_values:
+        #Initial guess
         a_guess = 0
         b_guess = 1
-        k = _k+1
-        [max_a, max_b] = scipy.optimize.fmin(log_likelihood,[a_guess, b_guess], args = (data[:k+1],))
+        
+        [max_a, max_b] = scipy.optimize.fmin(log_likelihood,[a_guess, b_guess], args = (data[:k],))
+        
         a_values.append(max_a)
         b_values.append(max_b)
     
@@ -97,12 +111,24 @@ def plot_likelihood_function_of_k(data,a,b):
     plt.legend(ncol=2)
     plt.xlabel('k')
     plt.ylabel('location (km)')
+    
+    #Print some values
+    print "Found a:", (a_values[-1])
+    print "Found b:", (b_values[-1])
+    print "True a - found a:", (a-a_values[-1])
+    print "True b - found b:", (b-b_values[-1])
+    print "True a:", (a)
+    print "True b:", (b)
 
 if __name__ == "__main__":
     np.random.seed(42)
+    
+    #a=3.74540, b=1.95071 with the above seed 42
     a,b = generate_random_position()
+    
     print "Lighthouse position: alpha=",a, "beta=",b
+    
     data = generate_data(a,b,200)
     plot_mean_over_time(data)
-    #plot_likelihood(data)
+    plot_likelihood(data)
     plot_likelihood_function_of_k(data,a,b)
